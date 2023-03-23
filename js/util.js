b78bdfbcafc16ba30e42f09ec024d5d760158b6c
MartinFiorde <fiorde_90@hotmail.com>
2023-03-22 23:09:46 -0300

    precarga con sweetAlert

--- js/util.js
+++ js/util.js
@@ -1,5 +1,6 @@
 import { Ejercicio, TarjetaEntrenamiento } from "./entidades.js";
 import { DateTime } from './librerias/luxon.js'; // LIBRERIA LUXON - https://moment.github.io/luxon/#/
+import { SweetAlert as swalImport } from './librerias/sweetalert2.js';
 
 // https://regexr.com/
 // http://w3.unpocodetodo.info/utiles/regex-en-javascript.php
@@ -7,6 +8,55 @@ import { DateTime } from './librerias/luxon.js'; // LIBRERIA LUXON - https://mom
 // https://stackoverflow.com/questions/32043294/regular-expression-for-y-yes-y-yes-1
 const CONFIRMAR = (option) => (/^(?:s(?:i)?)$/i.test(option)); // la misma logica que en la linea siguiente, pero sintetico con regex
 // const CONFIRMAR = (option) => (option == "s" || option == "S" || option == "si" || option == "Si" || option == "SI");
+
+
+
+const consultaPrecargaTarjetas = () => {
+    const baseStyle = swalImport.mixin({
+        customClass: {
+            confirmButton: 'btn',
+            cancelButton: 'btn'
+        },
+        buttonsStyling: false
+    })
+
+    baseStyle.fire({
+        title: 'Entrenamientos no encontrados',
+        text: "¿Desea recuperar los entrenamientos guardados en memoria?",
+        showCancelButton: true,
+        confirmButtonText: 'Aceptar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            respuestaPrecargaTarjetas(true);
+        } else {
+            respuestaPrecargaTarjetas(false);
+        }
+    });
+}
+
+const respuestaPrecargaTarjetas = (option) => {
+    if (option) {
+        let listaEjercicios = ejerciciosGuardados();
+        let listaTarjetasEntrenamiento = [
+            /*
+            // VERSION SIN LIBRERIA LUXON
+            new TarjetaEntrenamiento("d370ba91-a01f-482a-bd1c-5fb40322d148", listaEjercicios[0], "2023-12-30", (0 * 3600000 + 39 * 60000 + 20 * 1000), 272, 187, 6.26),
+            new TarjetaEntrenamiento("1dec0154-fec1-4d6d-b060-c5c04c50faec", listaEjercicios[1], "2023-12-23", (1 * 3600000 + 30 * 60000 + 0 * 1000), 414, 127, null),
+            new TarjetaEntrenamiento("444d7907-e8b6-47c7-b9a4-56fc5f10d269", listaEjercicios[2], "2023-12-15", (1 * 3600000 + 10 * 60000 + 30 * 1000), 398, 112, null),
+            new TarjetaEntrenamiento("1fc0bd84-bd3b-4b0c-80dc-821ff877c39b", listaEjercicios[3], "2023-11-28", (2 * 3600000 + 0 * 60000 + 0 * 1000), 398, 115, 13.62),
+            */
+            new TarjetaEntrenamiento("d370ba91-a01f-482a-bd1c-5fb40322d148", listaEjercicios[0], DateTime.fromFormat('2023-12-30', "yyyy-MM-dd").toISO(), (0 * 3600000 + 39 * 60000 + 20 * 1000), 272, 187, 6.26),
+            new TarjetaEntrenamiento("1dec0154-fec1-4d6d-b060-c5c04c50faec", listaEjercicios[1], DateTime.fromFormat('2023-12-23', "yyyy-MM-dd").toISO(), (1 * 3600000 + 30 * 60000 + 0 * 1000), 414, 127, null),
+            new TarjetaEntrenamiento("444d7907-e8b6-47c7-b9a4-56fc5f10d269", listaEjercicios[2], DateTime.fromFormat('2023-12-15', "yyyy-MM-dd").toISO(), (1 * 3600000 + 10 * 60000 + 30 * 1000), 398, 112, null),
+            new TarjetaEntrenamiento("1fc0bd84-bd3b-4b0c-80dc-821ff877c39b", listaEjercicios[3], DateTime.fromFormat('2023-11-28', "yyyy-MM-dd").toISO(), (2 * 3600000 + 0 * 60000 + 0 * 1000), 398, 115, 13.62),
+        ];
+        localStorage.setItem("listaTarjetas", JSON.stringify(listaTarjetasEntrenamiento));
+        location.reload();
+    } else {
+        localStorage.setItem("listaTarjetas", "[]");
+    }
+}
 
 export const cargaInicialLocalStorage = () => {
     if (localStorage.getItem("listaEjercicios") == null || localStorage.getItem("listaEjercicios") == "[]") {
@@ -18,29 +68,8 @@ export const cargaInicialLocalStorage = () => {
         ];
         localStorage.setItem("listaEjercicios", JSON.stringify(listaEjercicios));
     }
-
     if (localStorage.getItem("listaTarjetas") == null || localStorage.getItem("listaTarjetas") == "[]") {
-        let option = prompt("Detectamos que no tiene entrenamientos cargados. Desea recuperar los entrenamientos guardados en memoria? s/n")
-        if (CONFIRMAR(option)) {
-            let listaEjercicios = ejerciciosGuardados();
-            let listaTarjetasEntrenamiento = [
-                /*
-                // VERSION SIN LIBRERIA LUXON
-                new TarjetaEntrenamiento("d370ba91-a01f-482a-bd1c-5fb40322d148", listaEjercicios[0], "2023-12-30", (0 * 3600000 + 39 * 60000 + 20 * 1000), 272, 187, 6.26),
-                new TarjetaEntrenamiento("1dec0154-fec1-4d6d-b060-c5c04c50faec", listaEjercicios[1], "2023-12-23", (1 * 3600000 + 30 * 60000 + 0 * 1000), 414, 127, null),
-                new TarjetaEntrenamiento("444d7907-e8b6-47c7-b9a4-56fc5f10d269", listaEjercicios[2], "2023-12-15", (1 * 3600000 + 10 * 60000 + 30 * 1000), 398, 112, null),
-                new TarjetaEntrenamiento("1fc0bd84-bd3b-4b0c-80dc-821ff877c39b", listaEjercicios[3], "2023-11-28", (2 * 3600000 + 0 * 60000 + 0 * 1000), 398, 115, 13.62),
-                */
-                new TarjetaEntrenamiento("d370ba91-a01f-482a-bd1c-5fb40322d148", listaEjercicios[0], DateTime.fromFormat('2023-12-30', "yyyy-MM-dd").toISO(), (0 * 3600000 + 39 * 60000 + 20 * 1000), 272, 187, 6.26),
-                new TarjetaEntrenamiento("1dec0154-fec1-4d6d-b060-c5c04c50faec", listaEjercicios[1], DateTime.fromFormat('2023-12-23', "yyyy-MM-dd").toISO(), (1 * 3600000 + 30 * 60000 + 0 * 1000), 414, 127, null),
-                new TarjetaEntrenamiento("444d7907-e8b6-47c7-b9a4-56fc5f10d269", listaEjercicios[2], DateTime.fromFormat('2023-12-15', "yyyy-MM-dd").toISO(), (1 * 3600000 + 10 * 60000 + 30 * 1000), 398, 112, null),
-                new TarjetaEntrenamiento("1fc0bd84-bd3b-4b0c-80dc-821ff877c39b", listaEjercicios[3], DateTime.fromFormat('2023-11-28', "yyyy-MM-dd").toISO(), (2 * 3600000 + 0 * 60000 + 0 * 1000), 398, 115, 13.62),
-            ];
-            localStorage.setItem("listaTarjetas", JSON.stringify(listaTarjetasEntrenamiento));
-        } else {
-            localStorage.setItem("listaTarjetas", "[]");
-        }
-
+        consultaPrecargaTarjetas();
     }
 }
 
@@ -65,3 +94,4 @@ export const tarjetasEntrenamientoGuardadas = () => {
     }
     return listaObj;
 }
+
