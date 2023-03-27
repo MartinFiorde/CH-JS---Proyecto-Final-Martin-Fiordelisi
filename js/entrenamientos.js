7d5811f0ce3d402d14009a14b1c616d781619da3
MartinFiorde <fiorde_90@hotmail.com>
2023-03-27 13:02:17 -0300

    separada logica de generación de etiquetas HTML en clase aparte

--- js/entrenamientos.js
+++ js/entrenamientos.js
@@ -1,5 +1,6 @@
 import { TarjetaEntrenamiento } from "./entidades.js";
 import { cargaInicialLocalStorage, tarjetasEntrenamientoGuardadas } from "./util.js";
+import { generarTarjeta } from "./generadorHtml.js";
 
 const generarParrafo = (type, title, content) => {
     let main = document.createElement(type);
@@ -45,44 +46,11 @@ const cargarResumen = () => {
     resumen.append(generarParrafo("p", "Velocidad promedio no ponderada: ", `${controlDeValoresNulos(parseFloat((parseFloat(totalVelocidad) / parseFloat(cantidadVelocidad)).toFixed(2)))} km/h`));
 };
 
-const generarCarta = (aux) => {
-    let div = document.createElement("article");
-    div.classList.add("col-md-6");
-    let string = ``;
-    string = string.concat(`
-    <figure class="card__btn card__ flex__card row mb-4 g-0 border rounded overflow-hidden flex-md-row shadow-sm h-md-250 position-relative">
-        <figcaption class="card__text col p-2 pe-0 d-flex flex-column position-static">
-            <h2 class="card__title d-inline-block mb-0">${aux.idEjercicio.nombre}</h2>
-            <p class="mb-3 text-muted">${aux.mostrarDato("fecha")}</p>
-            <p class="card-text"><strong>Duración: </strong><br />${aux.mostrarDato("duracion")} hs</p>
-            <p class="card-text"><strong>Calorias: </strong><br />${aux.mostrarDato("calorias")} kcal</p>
-    `);
-    if (aux.idEjercicio.hasDistancia) {
-        string = string.concat(`
-            <p class="card-text"><strong>Vel. Prom.: </strong><br />${aux.mostrarDato("velocidadPromedio")} km/h</p>
-        `);
-    } else {
-        string = string.concat(`
-            <p class="card-text"><strong>Frec. Card. Prom.: </strong><br />${aux.mostrarDato("frecuenciaCardiacaPromedio")} lpm</p>
-        `);
-    }
-    string = string.concat(`
-            <a href="./ver-entrenamiento.html?id=${aux.mostrarDato("id")}" class="card__ stretched-link"></a> 
-        </figcaption>
-        <div class="card__img col-auto d-none d-lg-block">
-            <img class="card__img__content bd-placeholder-img rounded shadow-sm m-4" src="../img/entrenamientos/${aux.idEjercicio.urlImagen}" alt="icono ciclismo" aria-label="Placeholder: icono ciclismo" focusable="false" />
-        </div>
-    </figure>
-        `);
-    div.innerHTML = string;
-    return div;
-}
-
 const cargarTablero = () => {
     let listaEntrenamientosOBJ = tarjetasEntrenamientoGuardadas();
     const tablero = document.querySelector("[data-entrenamientos]");
     for (let i = 0; i < listaEntrenamientosOBJ.length; i++) {
-        tablero.append(generarCarta(listaEntrenamientosOBJ[i]));
+        tablero.append(generarTarjeta(listaEntrenamientosOBJ[i]));
     }
 }
 
--- js/verEntrenamiento.js
+++ js/verEntrenamiento.js
@@ -1,44 +1,15 @@
 import { cargaInicialLocalStorage, tarjetasEntrenamientoGuardadas } from "./util.js";
+import { verInnerHtml } from "./generadorHtml.js";
 import { SweetAlert as swalImport } from './librerias/sweetalert2.js';
 // const swalImport = Swal; //LIBRERIA PROMPTS - https://sweetalert2.github.io/
 // LIBRERIA PROMPTS SIMILAR - https://github.com/apvarun/toastify-js/blob/master/README.md
-
-const cartaInnerHtml = (aux) => {
-    let string = ``;
-    string = string.concat(`
-        <div class="card__img col-auto">
-            <img class="card__img__content bd-placeholder-img rounded shadow-sm m-4" src="../img/user_placeholder.png" alt="icono pfp foto de usuario" aria-label="Placeholder: foto de usuario" focusable="false" data-banner/>
-        </div>
-        <figcaption class="card__text col p-2 pe-0 d-flex flex-column position-static">
-            <h2 class="card__title d-inline-block mb-0" data-tipo>${aux.idEjercicio.nombre}</h2>
-            <p class="mb-3 text-muted">${aux.mostrarDato("fecha")}</p>
-            <p class="card-text py-0"><strong>Duración: </strong>${aux.mostrarDato("duracion")} hs</p>
-            <p class="card-text py-0"><strong>Calorías: </strong>${aux.mostrarDato("calorias")} kcal</p>
-            <p class="card-text py-0"><strong>Frecuencia cardíaca: </strong>${aux.mostrarDato("frecuenciaCardiacaPromedio")} lpm</p>
-    `);
-    if (aux.idEjercicio.hasDistancia) {
-        string = string.concat(`
-            <p class="card-text py-0"><strong>Distancia recorrida: </strong>${aux.mostrarDato("distancia")} km</p>
-            <p class="card-text py-0 pb-3"><strong>Velocidad promedio: </strong>${aux.mostrarDato("velocidadPromedio")} km/h</p>
-        `);
-    }
-    string = string.concat(`
-            <div class="btns">
-                <input class="btn" type="submit" value="Modificar" data-modificar/>
-                <input class="btn" type="submit" value="Eliminar" data-eliminar/>
-                <input class="btn" type="reset" value="Volver" data-volver/>
-            </div>
-        </figcaption>
-        `);
-    return string;
-}
 
 const cargaInfo = () => {
     const url = new URL(window.location);
     const idParam = url.searchParams.get("id");
     const aux = tarjetasEntrenamientoGuardadas().find(x => x.id == idParam);
     let info = document.querySelector("[data-info]");
-    info.innerHTML = cartaInnerHtml(aux);
+    info.innerHTML = verInnerHtml(aux);
     document.querySelector("[data-banner]").src = `../img/entrenamientos/${aux.idEjercicio.urlImagen}`;
     document.querySelector("[data-modificar]").addEventListener("click", () => window.location.href = `../pages/cargar-entrenamiento.html?id=${idParam}`);
     document.querySelector("[data-eliminar]").addEventListener("click", function () { confirmarBorrado(idParam) });
